ac55094948f1592d5197a3d2858580489d3b0c81
Bodify <bodifyttv@gmail.com>
2026-08-01 05:48:12 +0200

    fix castbar texture wrong when unint

--- midnight/BetterBlizzFrames.lua
+++ midnight/BetterBlizzFrames.lua
@@ -3744,12 +3744,35 @@ function BBF.HookUnitFrameTextures()
                 bg:SetPoint("BOTTOMRIGHT", bg:GetParent(), "BOTTOMRIGHT", 1, -1)
 
                 if not BBF.RecolorCastbarHooked then
+                    local green = CreateColor(0, 1, 0, 1)
+                    local yellow = CreateColor(1, 0.7, 0, 1)
+                    local unInt = CreateColor(0.7, 0.7, 0.7, 1)
                     statusBar:HookScript("OnEvent", function(self)
                         self:SetStatusBarTexture(castbarTexture)
+                        local notInterruptible
+                        local sbTex = self:GetStatusBarTexture()
                         if self.channeling then
-                            self:SetStatusBarColor(0, 1, 0)
+                            notInterruptible = select(7, UnitChannelInfo(statusBar.unit))
+                            if notInterruptible ~= nil then
+                                sbTex:SetVertexColorFromBoolean(
+                                    notInterruptible,
+                                    unInt,
+                                    green
+                                )
+                            else
+                                self:SetStatusBarColor(0, 1, 0)
+                            end
                         else
-                            self:SetStatusBarColor(1, 0.7, 0)
+                            notInterruptible = select(8, UnitCastingInfo(statusBar.unit))
+                            if notInterruptible ~= nil then
+                                sbTex:SetVertexColorFromBoolean(
+                                    notInterruptible,
+                                    unInt,
+                                    yellow
+                                )
+                            else
+                                self:SetStatusBarColor(1, 0.7, 0)
+                            end
                         end
                         -- if self.barType == "uninterruptable" then
                         --     self:SetStatusBarColor(0.7, 0.7, 0.7)
